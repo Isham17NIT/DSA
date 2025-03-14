032c33d491b6b883f7388eac2f7defd94d9f67b5
Isham <ishamahuja17@gmail.com>
2025-03-14 14:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 87.2 MB (40.83%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) 
+    {
+        int fives=0; //count of $5 coins
+        int tens=0; //count of $10 coins
+        // sort(bills.begin(),bills.end());
+        for(int i=0;i<bills.size();i++)
+        {
+            if(bills[i]==5)
+                fives++;
+            else if(bills[i]==10)
+            {
+                if(fives==0)
+                    return false;
+                else
+                    fives--;
+                tens++;
+            }
+            else //bills[i]=20 -->I'll try to give minm $5 coins-->so that they can be distributed to others
+            {
+                if(tens==0)
+                {
+                    if(fives<3)
+                        return false;
+                    else
+                        fives-=3;
+                }
+                else
+                {
+                    tens-=1;
+                    if(fives==0)
+                        return false;
+                    else
+                        fives-=1;
+                }
+            }
+        }
+        return true;
+    }
+};
