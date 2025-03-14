6c7095d11c433ca8bb6516a704088766294b2d32
Isham <ishamahuja17@gmail.com>
2025-03-14 23:11:51 +0530

    Time: 11 ms (17.47%) | Memory: 25.9 MB (7.55%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -3,46 +3,27 @@ class Solution
 public:
     bool isValidSudoku(vector<vector<char>>& board) 
     {
-        //row check
+        unordered_set<string> s;
         for(int i=0;i<9;i++)
         {
-            vector<bool> visited(9,false);
             for(int j=0;j<9;j++)
             {
-                if(board[i][j]!='.' && visited[board[i][j]-'0'-1])
-                    return false;
                 if(board[i][j]!='.')
-                    visited[board[i][j]-'0'-1]=true;
-            }
-        }
-        //col check
-        for(int j=0;j<9;j++)
-        {
-            vector<bool> visited(9,false);
-            for(int i=0;i<9;i++)
-            {
-                if(board[i][j]!='.' && visited[board[i][j]-'0'-1])
-                    return false;
-                if(board[i][j]!='.')
-                    visited[board[i][j]-'0'-1]=true;
-            }
-        }
-        //grid check
-        vector<vector<int>> gridStart = {{0,0},{0,3},{0,6},{3,0},{3,3},{3,6},{6,0},{6,3},{6,6}};
-        for(int k=0;k<9;k++)
-        {
-            vector<bool> visited(9,false);
-            int row=gridStart[k][0];
-            int col=gridStart[k][1];
-            for(int i=row;i<row+3;i++)
-            {
-                for(int j=col;j<col+3;j++)
                 {
-                    if(board[i][j]!='.' && visited[board[i][j]-'0'-1])
+                    string s1=to_string(board[i][j])+"at i="+to_string(i);
+                    string s2=to_string(board[i][j])+"at j="+to_string(j);
+                    int row=i/3;
+                    int col=j/3;
+                    string s3=to_string(board[i][j])+"at i="+to_string(row)+", j="+to_string(col);
+                    if(s.find(s1)==s.end() && s.find(s2)==s.end() && s.find(s3)==s.end())
+                    {
+                        s.insert(s1);
+                        s.insert(s2);
+                        s.insert(s3);
+                    }
+                    else
                         return false;
-                    if(board[i][j]!='.')
-                        visited[board[i][j]-'0'-1]=true;
-                }
+                }                
             }
         }
         return true;
