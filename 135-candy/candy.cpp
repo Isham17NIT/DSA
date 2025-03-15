e6aa7bbb24eb34544e5143fa0d6daa2abf7284bf
Isham <ishamahuja17@gmail.com>
2025-03-15 20:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (46.63%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -4,9 +4,7 @@ public:
     {
         int n=ratings.size();
         vector<int> left(n);
-        vector<int> right(n);
         left[0]=1;
-        right[n-1]=1;
         for(int i=1;i<n;i++)
         {
             if(ratings[i]>ratings[i-1])
@@ -14,19 +12,14 @@ public:
             else
                 left[i]=1; //greedily assign the minm possible
         }
+        int candies=0;
         for(int i=n-2;i>=0;i--)
         {
             if(ratings[i]>ratings[i+1])
-                right[i]=right[i+1]+1;
-            else
-                right[i]=1; //greedily assign the minm possible
-        }
-        int candies=0;
-        for(int i=0;i<n;i++)
-        {
-            left[i]=max(left[i],right[i]);
+                left[i]=max(left[i],left[i+1]+1);
             candies+=left[i];
         }
+        candies+=left[n-1];
         return candies;
     }
 };
