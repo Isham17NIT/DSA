47a0e039ffa133e9b5ec8915e814b3417f7f7da7
Isham <ishamahuja17@gmail.com>
2025-03-15 13:32:51 +0530

    Time: 50 ms (22.56%) | Memory: 21.6 MB (19.75%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,25 +1,18 @@
-class Solution 
-{
+class Solution {
 public:
-    int minJumps(vector<int>& nums,int idx,vector<int> &dp)
-    {
-        if(idx>=nums.size()-1)
-            return 0; //no further jump needed
-        if(dp[idx]!=-1)
-            return dp[idx];
-     
-        int ans=INT_MAX;
-        for(int i=1;i<=nums[idx];i++)
-        {
-            int curr=minJumps(nums,idx+i,dp);
-            if(curr!=INT_MAX)
-                ans=min(ans,1+curr);
-        }
-        return dp[idx]=ans;
-    }
     int jump(vector<int>& nums) 
     {
-        vector<int> dp(nums.size(),-1);
-        return minJumps(nums,0,dp);
+        vector<int> jumps(nums.size(),INT_MAX);
+        jumps[0]=0;
+        int i=0;
+        while(jumps[nums.size()-1]==INT_MAX)
+        {
+            for(int j=1;j<=nums[i] && i+j<nums.size();j++) //no. of jumps
+            {
+                jumps[i+j]=min(jumps[i+j],jumps[i]+1);
+            }
+            i++;
+        }
+        return jumps[nums.size()-1];
     }
 };
