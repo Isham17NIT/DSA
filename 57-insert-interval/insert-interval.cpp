c3b0caeb912686661933a5eee51e6def4fd628bb
Isham <ishamahuja17@gmail.com>
2025-03-16 11:51:24 +0530

    Time: 4 ms (18.95%) | Memory: 22.5 MB (6.40%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,11 +1,22 @@
-class Solution {
+class Solution 
+{
 public:
+    void insertAtCorrectPlace(vector<vector<int>> &arr,vector<int> &x)
+    {
+        int i=0;
+        while(i < arr.size() && arr[i][0] < x[0])
+        {
+            i++;
+        }
+        arr.insert(arr.begin()+i,x); 
+        return;
+    }
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
     {
         vector<vector<int>> ans;
-        unordered_set<int> overlapping;
+        unordered_set<int> overlapping; //stores indexes of intervals overlapping with newInterval
 
-        // find the minm start and maxm end time in case the overlapping intervals exist
+        // find the minm start and maxm end time in case of overlapping intervals so as to merge later
         int start=newInterval[0];
         int end=newInterval[1];
 
@@ -15,38 +26,33 @@ public:
             int end1=intervals[i][1];
             int start2=newInterval[0];
             int end2=newInterval[1];
-            if(start1>end2)
+
+            if(start1 > end2)
                 break;
-            else if((start1<=start2 && end1>=start2) || (start2<=start1 && end2>=start1)) //overlapping interval
+            else if((start1 <= start2 && end1 >= start2) || (start2 <= start1 && end2 >= start1)) //overlapping interval
             {
-                overlapping.insert(i); //index of overlapping interval
+                overlapping.insert(i); 
                 start=min(start,intervals[i][0]);
                 end=max(end,intervals[i][1]);
             }
         }
+
         if(overlapping.size()==0) //no overlapping intervals were found
         {
-            int i=0;
-            while(i < intervals.size() && intervals[i][0] < newInterval[0])
-            {
-                i++;
-            }
-            intervals.insert(intervals.begin()+i,newInterval);
+            insertAtCorrectPlace(intervals,newInterval);
             return intervals;
         }
-        //else
-        for(int i=0;i<intervals.size();i++)
+
+        //else-->some overlapping intervals present
+        for(int i=0;i<intervals.size();i++) //insert the non-overlapping intervals into ans
         {
             if(overlapping.find(i)==overlapping.end())
                 ans.emplace_back(intervals[i]);
         }
+
         vector<int> merged={start,end};
-        int i=0;
-        while(i < ans.size() && ans[i][0] < merged[0])
-        {
-            i++;
-        }
-        ans.insert(ans.begin()+i,merged);
+        insertAtCorrectPlace(ans,merged);
+
         return ans;
     }
 };
