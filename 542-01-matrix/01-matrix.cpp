810a99e33a9fce9d70157fb5a9a0a83de9119d57
Isham <ishamahuja17@gmail.com>
2025-03-22 13:20:03 +0530

    Time: 20 ms (54.94%) | Memory: 34.7 MB (84.02%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,7 +1,13 @@
 class Solution 
 {
 public:
-    void bfs(vector<vector<int>>&mat,int m,int n,vector<vector<bool>> &visited,queue<pair<int,int>> &q,vector<vector<int>>& distance)
+    int rows[4]={-1,0,1,0};
+    int cols[4]={0,1,0,-1};
+    bool isValid(int i,int j,int m,int n,vector<vector<int>>& mat,vector<vector<int>> &distance)
+    {
+        return i>=0 && i<m && j>=0 && j<n && mat[i][j]==1 && distance[i][j]==-1;
+    }
+    void bfs(vector<vector<int>>&mat,int m,int n,queue<pair<int,int>> &q,vector<vector<int>>& distance)
     {
         while(!q.empty())
         {
@@ -11,9 +17,8 @@ public:
             {
                 int newRow=curr.first+rows[i];
                 int newCol=curr.second+cols[i];
-                if(isValid(newRow,newCol,m,n,mat,visited))
+                if(isValid(newRow,newCol,m,n,mat,distance))
                 {
-                    visited[newRow][newCol]=true;
                     distance[newRow][newCol]=distance[curr.first][curr.second]+1;
                     q.push({newRow,newCol});
                 }
@@ -21,44 +26,30 @@ public:
         }
         return;
     }
-    int rows[4]={-1,0,1,0};
-    int cols[4]={0,1,0,-1};
-    bool isValid(int i,int j,int m,int n,vector<vector<int>>& mat,vector<vector<bool>> &visited)
-    {
-        return i>=0 && i<m && j>=0 && j<n && mat[i][j]==1 && !visited[i][j];
-    }
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
     {
         int m=mat.size();
         int n=mat[0].size();
        
         vector<vector<int>> distance(m,vector<int>(n,0));
-        vector<vector<bool>> visited(m,vector<bool>(n,false));
         queue<pair<int,int>> q;
 
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(mat[i][j]==0) //look for it's 4 directionally adjacent ones
+                if(mat[i][j]==0)
                 {
-                    visited[i][j]=true;
-                    for(int k=0;k<4;k++)
-                    {
-                        int newRow=i+rows[k];
-                        int newCol=j+cols[k];
-                        if(isValid(newRow,newCol,m,n,mat,visited))
-                        {
-                            visited[newRow][newCol]=true;
-                            distance[newRow][newCol]=1;
-                            q.push({newRow,newCol});
-                        }
-                    }
+                    q.push({i,j});
+                }
+                else
+                {
+                    distance[i][j]=-1;
                 }
             }
         }
 
-        bfs(mat,m,n,visited,q,distance);
+        bfs(mat,m,n,q,distance);
 
         return distance;
     }
