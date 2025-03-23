d7781cd651be4ce0ce5aeaadd75feda6ed270b16
Isham <ishamahuja17@gmail.com>
2025-03-23 16:55:58 +0530

    Time: 17 ms (64.31%) | Memory: 46.5 MB (65.83%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -22,10 +22,8 @@ public:
     }
     int makeConnected(int n, vector<vector<int>>& connections) 
     {
-        int cables=connections.size();
-        if(cables < n-1)
-            return -1;
-        int connected=1;
+        int extra=0; //no. of extra cables
+        int nC=0; //no. of components
         vector<int> parent(n);
         vector<int> size(n);
         for(int i=0;i<n;i++)
@@ -39,11 +37,21 @@ public:
             int v=e[1];
             int pu=findParent(parent,u);
             int pv=findParent(parent,v);
-            if(pu!=pv){
+            if(pu!=pv)
+            {
                 findUnion(pu,pv,parent,size);
-                connected++;
+            }
+            else
+            {
+                extra++;
             }
         }
-        return n-connected;
+        //find number of connected components
+        for(int i=0;i<n;i++)
+        {
+            if(parent[i]==i)
+                nC++;
+        }
+        return (extra>=nC-1) ? nC-1 : -1;
     }
 };
