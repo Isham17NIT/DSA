f262285b811f6cf954e24b29c7bede3022ef16f0
Isham <ishamahuja17@gmail.com>
2025-03-23 13:30:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (92.09%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -42,10 +42,9 @@ public:
             parent[i]=i;
             size[i]=0;
         }
-        vector<Edge> edges;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
                 if(isConnected[i][j]==1)
                 {
