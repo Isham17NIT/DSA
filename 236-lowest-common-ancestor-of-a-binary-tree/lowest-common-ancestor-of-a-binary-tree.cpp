b90602389c4b9e878db542d2b66e2a82cedabdce
Isham <ishamahuja17@gmail.com>
2025-03-30 15:40:48 +0530

    Time: 16 ms (27.61%) | Memory: 17.4 MB (41.72%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,42 +7,25 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution 
-{
+class Solution {
 public:
-    bool findPath(TreeNode* root, int x, vector<TreeNode*> &path)
+    TreeNode* findLCA(TreeNode* root, int p,int q)
     {
         if(!root)
-            return false;
-        path.push_back(root);
-        if(root->val==x) //node found
-            return true;
-        if(findPath(root->left, x, path) || findPath(root->right, x, path))
-            return true;
-
-        // node found in left and right part-->backtrack
-        path.pop_back();
-        return false;
+            return NULL;
+        if(root->val == p || root->val == q)
+            return root;
+        
+        TreeNode* l = findLCA(root->left, p, q);
+        TreeNode* r = findLCA(root->right, p, q);
+        if(!l)
+            return r;
+        if(!r)
+            return l;
+        return root;        
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
     {
-        vector<TreeNode*> path1; //p
-        vector<TreeNode*> path2; //q
-        findPath(root, p->val, path1);
-        findPath(root, q->val, path2);
-
-        int n = min(path1.size(),path2.size()) - 1;
-        TreeNode* lca=nullptr;
-
-        while(n>=0)
-        {
-            if(path1[n] == path2[n])
-            {
-                lca = path1[n];
-                break;
-            }
-            n--;
-        }
-        return lca;
+        return findLCA(root, p->val, q->val);
     }
 };
