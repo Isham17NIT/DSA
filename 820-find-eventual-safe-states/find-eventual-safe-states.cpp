005a029619a639f8214ef10fb05549804fb7607c
Isham <ishamahuja17@gmail.com>
2025-04-19 13:56:37 +0530

    Time: 44 ms (58.43%) | Memory: 66.2 MB (37.14%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,45 +1,48 @@
 class Solution 
 {
-private:
-    bool dfsVisit(int node,vector<vector<int>> &graph,vector<bool> &isSafe,vector<bool> &visited,vector<bool> &pathVisited)
-    {
-        visited[node] = true;
-        if(graph[node].empty()) //terminal node
-        {
-            isSafe[node] = true;
-            return false; //no cycle detected
-        }
-        pathVisited[node] = true;
-        for(int i : graph[node])
-        {
-            if(!visited[i])
-            {
-                if(dfsVisit(i,graph,isSafe,visited,pathVisited)) //cycle detected
-                    return true;
-            }
-            else if(pathVisited[i]) //condn of cycle
-                return true;
-        }
-        isSafe[node] = true;
-        pathVisited[node] = false;
-        return false; //no cycle detected
-    }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
     {
-        int v = graph.size();
-    
-        vector<bool> isSafe(v,false);
-        vector<int> ans;
-        vector<bool> visited(v,false);
-        vector<bool> pathVisited(v,false);
+        int v=graph.size();
 
+        vector<bool> isSafe(v,false);
+        vector<int> indegree(v,0);
+        vector<int> ans;
+        vector<vector<int>> revGraph(v,vector<int>());
         for(int i=0;i<v;i++)
         {
-            if(!visited[i]) 
-                dfsVisit(i,graph,isSafe,visited,pathVisited);
+            for(int j : graph[i])
+            {
+                revGraph[j].push_back(i);
+            }
         }
-
+        //compute indegrees
+        for(int i=0;i<v;i++)
+        {
+            for(int j : revGraph[i])
+            {
+                indegree[j]+=1;
+            }
+        }
+        
+        queue<int> q;
+        for(int i=0;i<v;i++)
+        {
+            if(indegree[i]==0)
+                q.push(i);
+        }
+        while(!q.empty())
+        {
+            int curr = q.front();
+            q.pop();
+            isSafe[curr] = true;
+            for(int j : revGraph[curr])
+            {
+                indegree[j]-=1;
+                if(indegree[j]==0)
+                    q.push(j);
+            }
+        }
         for(int i=0;i<v;i++)
         {
             if(isSafe[i])
