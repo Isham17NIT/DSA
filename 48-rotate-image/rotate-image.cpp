6a94703e1a0059afefcfcd0f8b0199a28ca45a79
Isham <ishamahuja17@gmail.com>
2025-05-21 23:51:14 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (91.81%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -7,28 +7,11 @@ public:
         if(n==1)
             return;
         //first find transpose
-        for(int i=1;i<n;i++)
+        for(int i=0;i<n;i++)
         {
-            int j=i; //row no
-            int k=0; //col no
-            int numOfSwaps = 0;
-            while(numOfSwaps < floor((i+1)/2))
+            for(int j=i+1;j<n;j++)
             {
-                swap(mat[j][k],mat[k][j]);
-                j--; k++;
-                numOfSwaps++;
-            }
-        }
-        for(int i=1;i<n-1;i++)
-        {
-            int j=i; //row no
-            int k=n-1; //col no
-            int numOfSwaps = 0;
-            while(numOfSwaps < floor((n-i)/2))
-            {
-                swap(mat[j][k],mat[k][j]);
-                j++; k--;
-                numOfSwaps++;
+                swap(mat[i][j],mat[j][i]);
             }
         }
         for(int j=0;j<n/2;j++)
