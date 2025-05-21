d83334fe1e22de075541a130609cf9c11eaf61ea
Isham <ishamahuja17@gmail.com>
2025-05-21 17:55:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (12.72%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,21 +3,14 @@ public:
     vector<vector<int>> generate(int numRows) 
     {
         vector<vector<int>> ans(numRows,vector<int>());
-        ans[0]={1};
-        if(numRows==1)
-            return ans;
-        ans[1]={1,1};
-        if(numRows>2)   
+        for(int row=0;row<numRows;row++)
         {
-            for(int row=2;row<numRows;row++)
+            ans[row]=vector<int>(row+1);
+            ans[row][0]=1;
+            ans[row][row]=1;
+            for(int i=1;i<row;i++)
             {
-                ans[row]=vector<int>(row+1);
-                ans[row][0]=1;
-                ans[row][row]=1;
-                for(int i=1;i<row;i++)
-                {
-                    ans[row][i]=ans[row-1][i-1] + ans[row-1][i];
-                }
+                ans[row][i]=ans[row-1][i-1] + ans[row-1][i];
             }
         }
         return ans;
