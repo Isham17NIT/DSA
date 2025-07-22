1eba318a0c6949e39d40d24f30f429d274703378
Isham <ishamahuja17@gmail.com>
2025-07-22 10:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (62.97%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -12,56 +12,22 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
     {
+        ListNode dummy;
+        ListNode* tail = &dummy;
         ListNode* temp1=list1, *temp2=list2;
-        ListNode* head=nullptr, *temp=nullptr;
         while(temp1 && temp2)
         {
             if(temp1->val < temp2->val){
-                if(!head){
-                    head = temp1;
-                    temp = temp1;
-                }
-                else{
-                    temp->next = temp1;
-                    temp = temp->next;
-                }
+                tail->next = temp1;
                 temp1 = temp1->next;
             }
-            else if(temp1->val >= temp2->val)
-            {
-                if(!head){
-                    head = temp2;
-                    temp = temp2;
-                }
-                else{
-                    temp->next = temp2;
-                    temp = temp->next;
-                }
+            else{
+                tail->next = temp2;
                 temp2 = temp2->next;
             }
+            tail = tail->next;
         }
-        while(temp1){
-            if(!head){
-                head = temp1;
-                temp = temp1;
-            }
-            else{
-                temp->next = temp1;
-                temp = temp->next;
-            }
-            temp1 = temp1->next;
-        }
-        while(temp2){
-            if(!head){
-                head = temp2;
-                temp = temp2;
-            }
-            else{
-                temp->next = temp2;
-                temp = temp->next;
-            }
-            temp2 = temp2->next;
-        }
-        return head;
+        tail->next = temp1!=nullptr ? temp1 : temp2;
+        return dummy.next; //dummy is the real head
     }
 };
