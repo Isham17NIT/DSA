84687b788274e9c36fdb0af0b30b4b1973444485
Isham <ishamahuja17@gmail.com>
2025-07-24 14:57:11 +0530

    Time: 3 ms (7.35%) | Memory: 8.9 MB (15.64%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -1,36 +1,31 @@
 class Solution {
-private:
-    bool measureWater(int a, int b, int x,int y, int target,vector<vector<bool>> &visited)
-    {
-        //explore all possibilities
-        if(a==target || b==target || a+b==target)
-            return true;
-        if(visited[a][b])
-            return false;
-        visited[a][b] = true;
-        if(measureWater(0, b, x, y, target, visited)) //empty jug1
-            return true;
-        if(measureWater(a, 0, x, y, target, visited)) //empty jug2
-            return true;
-        if(measureWater(x, b, x, y, target, visited)) //fill jug1
-            return true;
-        if(measureWater(a, y, x, y, target, visited)) //fill jug2
-            return true;
-            
-        //transfer water from jug1 to jug2
-        if(measureWater(max(0,a-(y-b)),min(y,a+b), x, y, target, visited))
-            return true;
-
-        //transfer water from jug2 to jug1
-        if(measureWater(min(x,a+b),max(0,b-(x-a)), x, y, target, visited))
-            return true;
-
-        return false;        
-    }
 public:
     bool canMeasureWater(int x, int y, int target) 
     {
         vector<vector<bool>> visited(x+1,vector<bool>(y+1,false));
-        return measureWater(x, y, x, y, target,visited);
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        visited[0][0] = true;
+        while(!q.empty())
+        {
+            auto [a,b] = q.front();
+            q.pop();
+            visited[a][b] = true;
+            if(a==target || b==target || a+b==target)
+                return true;
+            if(!visited[0][b])
+                q.push({0,b});
+            if(!visited[a][0])
+                q.push({a,0});
+            if(!visited[x][b])
+                q.push({x,b});
+            if(!visited[a][y])
+                q.push({a,y});
+            if(!visited[max(0,a-(y-b))][min(y,a+b)])
+                q.push({max(0,a-(y-b)), min(y,a+b)});
+            if(!visited[min(x,a+b)][max(0,b-(x-a))])
+                q.push({min(x,a+b), max(0,b-(x-a))});
+        }
+        return false;
     }
 };
