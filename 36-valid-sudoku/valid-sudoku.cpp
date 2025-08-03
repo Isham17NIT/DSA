6885632ccf9a049e984e0b359f5dc1d024e276e1
Isham <ishamahuja17@gmail.com>
2025-08-03 18:37:23 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (62.84%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,33 +1,41 @@
 class Solution 
 {
-public:
-    int getGridNumber(int r,int c)
+private:
+    int getGridNum(int i,int j)
     {
-        return r*3 + c;
+        return 3*(i/3) + (j/3);
     }
+    bool validate(int num,vector<int> rows,vector<int> cols,vector<int> grids,int rowNo,int colNo,int gridNo)
+    {
+        int mask=(1<<(num-1));
+        if(rows[rowNo] & mask)
+            return false;
+        else if(cols[colNo] & mask)
+            return false;
+        else if(grids[gridNo] & mask)
+            return false;
+        return true;
+    }
+public:
     bool isValidSudoku(vector<vector<char>>& board) 
     {
-        vector<short> row(9,0);
-        vector<short> col(9,0);
-        vector<short> grid(9,0);
+        vector<int> rows(9,0);
+        vector<int> cols(9,0);
+        vector<int> grids(9,0);
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
-                if(board[i][j]!='.')
-                {
-                    int bitmask = 1 << (board[i][j]-'0'-1); //0 based indexing
-
-                    int k=getGridNumber(i/3,j/3);
-
-                    if(row[i] & bitmask || col[j] & bitmask || grid[k] & bitmask)
-                        return false;
-                    
-                    //mark the current number as visited
-                    row[i] |= bitmask;
-                    col[j] |= bitmask;
-                    grid[k] |= bitmask;
-                }                
+                if(board[i][j]=='.')
+                    continue;
+                int num=board[i][j]-'0';
+                int gridNum=getGridNum(i,j);
+                if(!validate(num,rows,cols,grids,i,j,gridNum))
+                    return false;
+                int mask=(1<<(num-1));
+                rows[i] |= mask;
+                cols[j] |= mask;
+                grids[gridNum] |= mask;
             }
         }
         return true;
