b9c74f9e5e1a7f6914eb7be403029ad8fb1e19e8
Isham <ishamahuja17@gmail.com>
2025-08-05 21:39:47 +0530

    Time: 73 ms (58.43%) | Memory: 141 MB (59.11%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -2,33 +2,31 @@ class Solution
 {
 private:
     struct Compare{
-        vector<int>& arr1;
-        vector<int>& arr2;
-        Compare(vector<int>& nums1, vector<int>& nums2) : arr1(nums1), arr2(nums2) {}
-        bool operator()(pair<int,int> &a, pair<int,int> &b)
+        bool operator()(pair<int,pair<int,int>> &a, pair<int,pair<int,int>> &b)
         {
-            return arr1[a.first]+arr2[a.second] > arr1[b.first]+arr2[b.second]; //min heap based on sum
+            return a.first > b.first; //min heap based on sum
         }
     };
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
     {
         vector<vector<int>> ans;
-        Compare comp(nums1, nums2);
-        priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq(comp);
+        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, Compare> pq;
         set<pair<int,int>> visited;
-        pq.push({0,0});
+        pq.push({nums1[0]+nums2[0],{0,0}});
         while(k--)
         {
-            pair<int,int> p=pq.top();
+            pair<int,int> p=pq.top().second;
             pq.pop();
             ans.push_back({nums1[p.first], nums2[p.second]});
             if(p.first+1 < nums1.size() && visited.find({p.first+1, p.second})==visited.end()){
-                pq.push({p.first+1, p.second});
+                int sum=nums1[p.first+1]+nums2[p.second];
+                pq.push({sum,{p.first+1, p.second}});
                 visited.insert({p.first+1, p.second});
             }                
             if(p.second+1 < nums2.size() && visited.find({p.first, p.second+1})==visited.end()){
-                pq.push({p.first, p.second+1});
+                int sum=nums1[p.first]+nums2[p.second+1];
+                pq.push({sum,{p.first, p.second+1}});
                 visited.insert({p.first, p.second+1});
             }
         }
