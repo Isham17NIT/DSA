082518aebe6c5de1e92882a8d33b96083e658f93
Isham <ishamahuja17@gmail.com>
2025-08-09 23:25:37 +0530

    Time: 1561 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,16 +1,10 @@
 
-
-with
-    cte1 as (
-        select p1.product_id, p1.new_price as price
-        from products as p1
-        where change_date = (select max(p2.change_date) from products as p2
-                             where change_date<='2019-08-16' and p1.product_id = p2.product_id)
-    ),
-    cte2 as (
-        select distinct product_id, 10 as price
-        from products where product_id not in (select product_id from cte1)
-    )
-    select * from cte1
-    union
-    select * from cte2;
+select p1.product_id, COALESCE(p2.latest_price,10) as price
+from (select distinct product_id from products) as p1
+left join (
+    select p3.product_id, p3.new_price as latest_price
+    from products as p3
+    where p3.change_date = (select max(p4.change_date) from products as p4
+                            where p4.change_date<='2019-08-16' and p4.product_id=p3.product_id)
+) as p2
+on p1.product_id = p2.product_id;
