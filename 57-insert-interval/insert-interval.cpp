c70dc95753a730f38e35d0e495498403317eb3c3
Isham <ishamahuja17@gmail.com>
2025-08-10 11:41:22 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (73.79%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,33 +1,37 @@
 class Solution 
 {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) 
+    vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int>& newInterval) 
     {
         vector<vector<int>> ans;
-        
+        if(intervals.size()==0){
+            ans.push_back(newInterval);
+            return ans;
+        }
         int i=0;
-        int n=intervals.size();
-        while(i < n && intervals[i][1] < newInterval[0])
+        while(i < intervals.size() && intervals[i][0] <= newInterval[0])
         {
-            ans.emplace_back(intervals[i]);
+            ans.push_back(intervals[i]);
             i++;
         }
-
-        while(i < n && intervals[i][0] <= newInterval[1]) //overlapping intervals
-        // find min start time and maxm end time
+        if(!ans.empty() && newInterval[0] <= ans[ans.size()-1][1]) //can be merged
         {
-            newInterval[0]=min(newInterval[0],intervals[i][0]); 
-            newInterval[1]=max(newInterval[1],intervals[i][1]);
+            ans[ans.size()-1][0] = min(ans[ans.size()-1][0], newInterval[0]);
+            ans[ans.size()-1][1] = max(ans[ans.size()-1][1], newInterval[1]);
+        }
+        else //can't be merged
+            ans.push_back(newInterval);
+        while(i < intervals.size())
+        {
+            if(intervals[i][0] <= ans[ans.size()-1][1]) //can be merged
+            {
+                ans[ans.size()-1][0] = min(ans[ans.size()-1][0], intervals[i][0]);
+                ans[ans.size()-1][1] = max(ans[ans.size()-1][1], intervals[i][1]);
+            }
+            else
+                ans.push_back(intervals[i]);
             i++;
         }
-        ans.emplace_back(newInterval);
-        
-        while(i<n)
-        {
-            ans.emplace_back(intervals[i]);
-            i++;
-        }       
-
         return ans;
     }
 };
