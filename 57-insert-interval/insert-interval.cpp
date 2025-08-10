7ddd5ac5f4eb0a24c4e6c38c976a333d2005aeae
Isham <ishamahuja17@gmail.com>
2025-08-10 11:50:53 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (44.20%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -4,32 +4,26 @@ public:
     vector<vector<int>> insert(vector<vector<int>> &intervals, vector<int>& newInterval) 
     {
         vector<vector<int>> ans;
-        if(intervals.size()==0){
+        int n=intervals.size();
+        if(n==0){
             ans.push_back(newInterval);
             return ans;
         }
         int i=0;
-        while(i < intervals.size() && intervals[i][0] <= newInterval[0])
+        while(i < n && intervals[i][1] < newInterval[0]) //strictly no overlapping
         {
             ans.push_back(intervals[i]);
             i++;
         }
-        if(!ans.empty() && newInterval[0] <= ans[ans.size()-1][1]) //can be merged
+        while(i < n && intervals[i][0] <= newInterval[1]) //intervals overlapping
         {
-            ans[ans.size()-1][0] = min(ans[ans.size()-1][0], newInterval[0]);
-            ans[ans.size()-1][1] = max(ans[ans.size()-1][1], newInterval[1]);
+            newInterval[0]=min(newInterval[0], intervals[i][0]);
+            newInterval[1]=max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        else //can't be merged
-            ans.push_back(newInterval);
-        while(i < intervals.size())
-        {
-            if(intervals[i][0] <= ans[ans.size()-1][1]) //can be merged
-            {
-                ans[ans.size()-1][0] = min(ans[ans.size()-1][0], intervals[i][0]);
-                ans[ans.size()-1][1] = max(ans[ans.size()-1][1], intervals[i][1]);
-            }
-            else
-                ans.push_back(intervals[i]);
+        ans.push_back(newInterval);
+        while(i < n){
+            ans.push_back(intervals[i]);
             i++;
         }
         return ans;
