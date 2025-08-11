a1febc8b04340f98c91169f212e1f4eb3dbea9d8
Isham <ishamahuja17@gmail.com>
2025-08-11 15:37:44 +0530

    Time: 4 ms (11.36%) | Memory: 11 MB (23.28%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,38 +1,23 @@
-class Solution 
-{
-private:
-    void appendRemaining(string s, int idx, string &ans, int carry)
-    {
-        for(int i=idx;i>=0;i--)
-        {
-            int sum = carry + (s[i]-'0');
-            ans = char(sum%2 + 48) + ans;
-            carry = sum > 1 ? 1 : 0;
-        }
-        if(carry==1)
-            ans = '1' + ans;
-    }
+class Solution {
 public:
     string addBinary(string a, string b) 
     {
-        int l1=a.length(), l2=b.length();
+        int i=a.length()-1, j=b.length()-1;
+        int carry=0;
         string ans="";
-        int carry=0, i=0;
-        for(i=0;i<min(l1,l2);i++)
+        while(i>=0 || j>=0 || carry==1)
         {
-            int c1=a[l1-1-i]-'0';
-            int c2=b[l2-1-i]-'0';
-            int sum=c1+c2+carry;
+            int sum=carry;
+            if(i>=0){
+                sum+=(a[i]-'0');
+                i--;
+            }
+            if(j>=0){
+                sum+=(b[j]-'0');
+                j--;
+            }
             ans = char(sum%2 + 48) + ans;
             carry = sum > 1 ? 1 : 0;
-        }
-        if(l1 > l2)
-            appendRemaining(a, l1-l2-1, ans, carry);
-        else if(l1 < l2)
-            appendRemaining(b, l2-l1-1, ans, carry);
-        else{
-            if(carry==1)
-                ans='1'+ans;
         }
         return ans;
     }
