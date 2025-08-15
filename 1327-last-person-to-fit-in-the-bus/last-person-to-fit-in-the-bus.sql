49cfcc349f5d7feb586b327fe96b9255871dc49b
Isham <ishamahuja17@gmail.com>
2025-08-15 23:48:41 +0530

    Time: 894 ms (45.15%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
-with 
-    cte1 as (
-        select person_name, sum(weight) over(
-            order by turn asc
-        ) as cum_sum
-        from queue
-    )
-    select person_name
-    from cte1 
-    where cum_sum = (select max(cum_sum)
-                     from cte1 
-                     where cum_sum<=1000);
+select person_name
+from (
+    select person_name, sum(weight) over(
+        order by turn asc
+    ) as cum_sum
+    from queue) as t
+    where cum_sum<=1000
+    order by cum_sum desc
+    limit 1;
