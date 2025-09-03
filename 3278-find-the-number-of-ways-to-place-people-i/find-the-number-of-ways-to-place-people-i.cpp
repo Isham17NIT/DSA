1048be9032a944cb2f01d69db9eafabd625c021c
Isham <ishamahuja17@gmail.com>
2025-09-03 21:17:17 +0530

    Time: 7 ms (65.94%) | Memory: 32.3 MB (97.83%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -11,22 +11,15 @@ public:
         for(int i=0;i<points.size();i++)
         {
             int x1=points[i][0], y1=points[i][1];
+            int maxY=INT_MIN;
             for(int j=i+1;j<points.size();j++)
             {
                 int x2=points[j][0], y2=points[j][1];
-                if(x2>=x1 && y2<=y1)
-                {
-                    int c=0;
-                    for(vector<int> p : points)
-                    {
-                        if((p[0]==x1 && p[1]==y1) || (p[0]==x2 && p[1]==y2))
-                            continue;
-                        else if(p[0]>=x1 && p[0]<=x2 && p[1]<=y1 && p[1]>=y2)
-                            c+=1;
-                    }
-                    if(c==0) //valid pair
-                        cnt+=1;
-                }
+                if(y2>y1)
+                    continue;
+                if(maxY==INT_MIN || y2>maxY)
+                    cnt++;
+                maxY=max(maxY,y2);
             }
         }
         return cnt;
