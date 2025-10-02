194d66e18404364ad6946c85ecf6955129a1ee05
Isham <ishamahuja17@gmail.com>
2025-10-02 11:29:10 +0530

    Time: 16 ms (64.79%) | Memory: 43.9 MB (61.73%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,61 +1,24 @@
 class MyCalendar {
 private:
-    struct Node{
-        pair<int,int> val;
-        Node* next, *prev;
-        Node(pair<int,int> val_){
-            val=val_;
-            next=prev=nullptr;
-        }
-    };
-    Node* root;
+    set<pair<int,int>> calendar;
 public:
-    MyCalendar() 
-    {
-        root=nullptr;
+    MyCalendar() {
+        calendar=set<pair<int,int>>();
     }
     
     bool book(int startTime, int endTime) 
     {
-        if(!root){
-            root=new Node({startTime, endTime});
-            return true;
-        }
-        Node* temp=root;
-        while(temp)
-        {
-            int begin=temp->val.first;
-            int end=temp->val.second;
-            if(endTime<=begin || startTime>=end) // no overlapping
-            {
-                if(startTime > begin)
-                {
-                    if(temp->next){
-                        temp=temp->next;
-                        continue;
-                    }
-                    Node* newNode=new Node({startTime, endTime});
-                    newNode->prev=temp;
-                    temp->next=newNode;
-                    return true;
-                }
-                Node* newNode=new Node({startTime, endTime});
-                newNode->next=temp;
-                if(temp->prev){
-                    newNode->prev=temp->prev;
-                    newNode->prev->next=newNode;
-                }
-                else{
-                    root=newNode;
-                }
-                newNode->next=temp;
-                temp->prev=newNode;
-                return true;
-            }
-            else //overlapping
+        pair<int,int> event={startTime, endTime};
+        const auto nextEvent=calendar.lower_bound(event);
+        if(nextEvent!=calendar.end() && nextEvent->first < endTime) //overlapping
+            return false;
+        if(nextEvent!=calendar.begin()){
+            const auto prevEvent=prev(nextEvent);
+            if(prevEvent->second > startTime) //overlapping
                 return false;
         }
-        return false;
+        calendar.insert(event);
+        return true;
     }
 };
 
