1a0965fd5f785741f8432c8829c550ffbbec7b66
Isham <ishamahuja17@gmail.com>
2025-10-03 15:59:17 +0530

    Time: 1 ms (92.41%) | Memory: 15.2 MB (67.18%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -4,21 +4,6 @@ public:
     bool repeatedSubstringPattern(string s) 
     {
         int n=s.length();
-        int l=n/2; //maxm length of substr ---> corr. to minm 2 repetitions
-        for(int i=1;i<=l;i++)
-        {
-            if(n%i==0)
-            {
-                string str=s.substr(0,i);
-                string tmp="";
-                for(int j=0;j<n/i;j++)
-                {
-                    tmp+=str;
-                }
-                if(tmp==s)
-                    return true;
-            }
-        }
-        return false;
+        return (s+s).substr(1,n*2-2).find(s)!=-1;
     }
 };
