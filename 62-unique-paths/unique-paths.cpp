c0067004d285fb16bc078d917a695128954b35ea
Isham <ishamahuja17@gmail.com>
2025-10-07 20:27:58 +0530

    Time: 2 ms (18.64%) | Memory: 8.4 MB (73.25%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,22 +1,28 @@
-class Solution 
-{
+class Solution {
+private:
+    bool isValid(int i,int j,int m,int n)
+    {
+        return i>=0 && j>=0 && i<m && j<n;
+    }
 public:
     int uniquePaths(int m, int n) 
     {
-        vector<int> prevRow(n,1);
-        vector<int> currRow(n,0);
-
-        for(int i=1;i<m;i++)
+        vector<int> prev(n,-1);
+        for(int i=0;i<m;i++)
         {
+            vector<int> curr(n,-1);
             for(int j=0;j<n;j++)
             {
-                if(j==0)
-                    currRow[j] = prevRow[j];
-                else
-                    currRow[j] = currRow[j-1] + prevRow[j];
+                if(i==0 && j==0){
+                    curr[j]=1;
+                    continue;
+                }
+                int up=isValid(i-1,j,m,n) ? prev[j] : 0;
+                int left=isValid(i,j-1,m,n) ? curr[j-1] : 0;
+                curr[j]=up+left;
             }
-            prevRow = currRow;
+            prev=curr;
         }
-        return prevRow[n-1];
+        return prev[n-1];
     }
 };
