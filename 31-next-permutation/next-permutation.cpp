e3121140628b1ccaeaf64d77f02377cee5d5a324
Isham <ishamahuja17@gmail.com>
2025-10-20 11:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (32.67%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,8 +1,46 @@
 // brute force
 class Solution {
+private:
+    int findBreakpoint(vector<int> &nums)
+    {
+        int i=nums.size()-2;
+        while(i>=0 && nums[i]>=nums[i+1])
+        {
+            i--;
+        }
+        return i;
+    }
+    int findJustLargerThanBreakpt(vector<int> &nums, int i)
+    {
+        int j=nums.size()-1;
+        while(j>i && nums[j]<=nums[i])
+        {
+            j--;
+        }
+        return j;
+    }
+    void reverseArray(vector<int> &nums, int start)
+    {
+        int i=start, j=nums.size()-1;
+        while(i<j)
+        {
+            swap(nums[i],nums[j]);
+            i++; j--;
+        }
+    }
 public:
     void nextPermutation(vector<int>& nums) 
     {
-        next_permutation(nums.begin(), nums.end());
+        int i=findBreakpoint(nums);
+        if(i==-1) //largest permutation--->descending order of elements
+        {
+            sort(nums.begin(),nums.end());
+            return;
+        }
+
+        int j=findJustLargerThanBreakpt(nums, i);
+        swap(nums[i],nums[j]);
+
+        reverseArray(nums, i+1);
     }
 };
