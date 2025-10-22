027661cca90411354fc95ebf4c8adff8ec54f6ea
Isham <ishamahuja17@gmail.com>
2025-10-22 11:12:08 +0530

    Time: 301 ms (17.97%) | Memory: 91.2 MB (52.72%) - LeetSync

--- 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
+++ 1747-lexicographically-smallest-string-after-applying-operations/lexicographically-smallest-string-after-applying-operations.cpp
@@ -1,31 +1,34 @@
 class Solution 
 {
-private:
-    void findLexSmallestStringHelper(string str, int a, int b, set<string> &s)
-    {
-        if(s.find(str)!=s.end())
-            return;
-        // insert the curr string
-        s.insert(str);
-        //rotate
-        string rotatedStr=str.substr(str.size()-b,b)+str.substr(0,str.size()-b);
-        findLexSmallestStringHelper(rotatedStr, a, b, s);
-        //add a
-        string addedStr=str;
-        for(int i=1;i<str.size();i+=2)
-        {
-            int currChar=str[i]-'0';
-            addedStr[i]=((currChar+a)%10)+'0';
-        }
-        findLexSmallestStringHelper(addedStr, a, b, s);
-        return;
-    } 
 public:
     string findLexSmallestString(string str, int a, int b) 
     {
-        set<string> s;
-        findLexSmallestStringHelper(str, a, b, s);
-        auto it=s.begin();
-        return *it;
+        int n=str.size();
+        string ans=str;
+        set<string> visited; //to keep track of visited states
+        queue<string> q;
+        q.push(str);
+        while(!q.empty())
+        {
+            string curr=q.front();
+            q.pop();
+            ans=min(ans, curr);
+            string rotatedStr=curr.substr(n-b)+curr.substr(0,n-b);
+            if(visited.find(rotatedStr)==visited.end())
+            {
+                q.push(rotatedStr);
+                visited.insert(rotatedStr);
+            }
+            for(int i=1;i<n;i+=2)
+            {
+                curr[i]=(curr[i]-'0'+a)%10 + '0';
+            }
+            if(visited.find(curr)==visited.end())
+            {
+                q.push(curr);
+                visited.insert(curr);
+            }
+        }
+        return ans;
     }
 };
