60c25d54e7cea62d94bdded2dc1639f757bb7683
Isham <ishamahuja17@gmail.com>
2025-10-25 11:28:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (13.49%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -4,19 +4,8 @@ public:
     {
         int numWeeks=n/7;
         int extraDays=n%7;
-        int totalMoney = 0;
-        int prev=numWeeks+1;
-        while(numWeeks>0)
-        {   
-            totalMoney+=(3+numWeeks)*7;
-            numWeeks--;
-        } 
-        while(extraDays>0)
-        {
-            totalMoney+=prev;
-            prev+=1;
-            extraDays--;
-        }
+        int totalMoney = 7*(3*numWeeks + numWeeks*(numWeeks+1)/2);
+        totalMoney+=(numWeeks*extraDays + extraDays*(extraDays+1)/2);
         return totalMoney;
     }
 };
