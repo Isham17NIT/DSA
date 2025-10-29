f89b16f804c1e24a936419b75d8f82343b0cccb7
Isham <ishamahuja17@gmail.com>
2025-10-29 16:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (89.59%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -2,29 +2,11 @@ class Solution {
 public:
     int smallestNumber(int n) 
     {
-        int orig=n;
-        int i=floor(log2(n)); // i+1 is the number of bits in binary rep
-        vector<int> powers(i+1); // stores powers of 2
-        vector<int> unsetBits; // bit posn where bit is unset
-
-        powers[0]=1;
-        for(int k=1;k<=i;k++)
+        int x=1;
+        while(x<n)
         {
-            powers[k]=powers[k-1]*2;
+            x=x*2 + 1;
         }
-        while(i>=0)
-        {
-            if(n>=powers[i])
-                n%=powers[i];
-            else
-                unsetBits.push_back(i);
-            i--;
-        }
-        for(int k: unsetBits)
-        {
-            orig+=powers[k];
-        }
-        return orig;        
+        return x;
     }
 };
-
