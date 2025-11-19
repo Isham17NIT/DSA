2919dd11f69124ae151c7d4dac14e55e72eb2469
Isham <ishamahuja17@gmail.com>
2025-11-19 19:13:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (33.10%) - LeetSync

---
+++ 1636-number-of-substrings-with-only-1s/number-of-substrings-with-only-1s.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int numSub(string s) 
+    {
+        int ans=0, mod=1e9+7;
+        int prevIdx=-1; //idx of starting 1 in the current substring 
+        for(int i=0;i<s.length();i++)
+        {
+            if(prevIdx==-1)
+            {
+                if(s[i]=='1')
+                    prevIdx=i;
+                else //s[i]=='0'
+                    continue;
+            } 
+            else if(s[i]=='0')
+            {
+                int n=i-prevIdx;  // no. of consecutive ones
+                ans=(ans+(long long)n*(long long)(n+1)/2)%mod;
+                prevIdx=-1;
+            }
+            else //s[i]=='1'
+                continue;
+        }
+        if(s[s.length()-1]=='1')
+        {
+            if(prevIdx==-1)
+                ans=(ans+1)%mod;
+            else
+            {
+                int n=s.length()-prevIdx;  // no. of consecutive ones
+                ans=(ans+(long long)n*(long long)(n+1)/2)%mod;
+            }
+        }
+        return ans;
+    }
+};
