079aad13a5aea7999815ec9a6920b7bd338c3e46
Isham <ishamahuja17@gmail.com>
2026-01-02 13:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (49.91%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
@@ -2,27 +2,12 @@ class Solution {
 public:
     int fillCups(vector<int>& amount) 
     {
-        int ans=0;
-        priority_queue<int> pq; //max heap
+        int maxCups=0, totalCups=0;
         for(int i : amount)
         {
-            if(i>0)
-                pq.push(i);
+            maxCups = max(maxCups, i);
+            totalCups+=i;
         }
-        while(!pq.empty())
-        {
-            int x = pq.top();
-            pq.pop();
-            if(pq.empty())
-                return ans + x;
-            int y = pq.top();
-            pq.pop();
-            if(x>1)
-                pq.push(x-1);
-            if(y>1)
-                pq.push(y-1);
-            ans++;
-        }
-        return ans;
+        return max(maxCups, (totalCups+1)/2);
     }
 };
