20b801372f55db0d99ba120a4272823227139b65
Isham <ishamahuja17@gmail.com>
2026-01-03 17:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (55.60%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -4,31 +4,25 @@ public:
     int maxDistToClosest(vector<int>& seats) 
     {
         int ans=0, n=seats.size();
-        int leftClosest = -1;
-        vector<int> rightClosest(n,-1);
-    
-        for(int i=n-1;i>=0;i--)
-        {
-            if(seats[i]==1)
-                rightClosest[i]=i;
-            else{
-                if(i==n-1)
-                    rightClosest[i]=INT_MAX;
-                else
-                    rightClosest[i]=rightClosest[i+1];
-            }
-        }
+        int prevFilled=-1;
         for(int i=0;i<n;i++)
         {
-            if(seats[i]==1)  //occupied
-            {
-                leftClosest=i;
+            if(seats[i]==0)
                 continue;
+            if(prevFilled==-1) //first filled seat
+            {
+                prevFilled=i;
+                ans = max(ans, i);
             }
-            int leftDis = leftClosest==-1 ? INT_MAX : i - leftClosest;
-            int rightDis = rightClosest[i]==INT_MAX ? INT_MAX : rightClosest[i] - i;
-            ans = max(ans, min(leftDis,rightDis));
+            else
+            {
+                int k = i-prevFilled-1;
+                ans = max(ans, (k+1)/2);
+                prevFilled=i;
+            }
         }
+        if(prevFilled < n)
+            ans=max(ans, n-prevFilled-1);
         return ans;
     }
 };
