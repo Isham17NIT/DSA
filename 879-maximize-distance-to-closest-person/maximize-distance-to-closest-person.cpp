ab2faa9be74959c49111abd49b3f5e93448655a8
Isham <ishamahuja17@gmail.com>
2026-01-03 16:51:00 +0530

    Time: 4 ms (11.80%) | Memory: 23.7 MB (17.20%) - LeetSync

--- 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
+++ 879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.cpp
@@ -4,19 +4,9 @@ public:
     int maxDistToClosest(vector<int>& seats) 
     {
         int ans=0, n=seats.size();
-        vector<int> leftClosest(n,-1);
+        int leftClosest = -1;
         vector<int> rightClosest(n,-1);
-        for(int i=0;i<n;i++)
-        {
-            if(seats[i]==1)
-                leftClosest[i]=i;
-            else{
-                if(i==0)
-                    leftClosest[i]=INT_MAX;
-                else
-                    leftClosest[i]=leftClosest[i-1];
-            }
-        }
+    
         for(int i=n-1;i>=0;i--)
         {
             if(seats[i]==1)
@@ -31,8 +21,11 @@ public:
         for(int i=0;i<n;i++)
         {
             if(seats[i]==1)  //occupied
+            {
+                leftClosest=i;
                 continue;
-            int leftDis = leftClosest[i]==INT_MAX ? INT_MAX : i - leftClosest[i];
+            }
+            int leftDis = leftClosest==-1 ? INT_MAX : i - leftClosest;
             int rightDis = rightClosest[i]==INT_MAX ? INT_MAX : rightClosest[i] - i;
             ans = max(ans, min(leftDis,rightDis));
         }
