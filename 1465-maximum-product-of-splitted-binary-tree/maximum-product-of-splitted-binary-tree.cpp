07237a907132d84e35bb8ee8d60f9ff791f56415
Isham <ishamahuja17@gmail.com>
2026-01-07 13:06:02 +0530

    Time: 0 ms (100.00%) | Memory: 83.3 MB (15.96%) - LeetSync

--- 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
+++ 1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -9,38 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution 
-{
+class Solution {
 private:
-    int mod=1e9+7;
-    long long maxProd=0;
-
-    long long getSum(TreeNode* root)
+    long long inorderTraversal(TreeNode* root, vector<long long> &subtreeSums)
     {
         if(!root)
             return 0;
-        long long lSum = getSum(root->left);
-        long long rSum = getSum(root->right);
-        return lSum + rSum + root->val;
-    }
-    long long calculateMaxProduct(TreeNode* root, long long &totalSum)
-    {
-        if(!root)
-            return 0;
-        long long lSum = calculateMaxProduct(root->left, totalSum);
-        long long rSum = calculateMaxProduct(root->right, totalSum);
-        // try splitting left edge
-        maxProd = max(maxProd, lSum * (totalSum-lSum));
-        // try splitting right edge
-        maxProd = max(maxProd, rSum * (totalSum-rSum));
-        return lSum + rSum + root->val;
+        long long subTreeSum = root->val + inorderTraversal(root->left, subtreeSums) + inorderTraversal(root->right, subtreeSums);
+        subtreeSums.push_back(subTreeSum);
+        return subTreeSum;
     }
 public:
     int maxProduct(TreeNode* root) 
     {
-        long long totalSum=getSum(root); //total sum of tree
-
-        calculateMaxProduct(root, totalSum);
+        vector<long long> subtreeSums;
+        long long maxProd=0;
+        int mod=1e9+7;
+        long long totalSum = inorderTraversal(root, subtreeSums);
+        for(long long i=0;i<subtreeSums.size()-1;i++)
+        {
+            // try cutting the edge above this subtree
+            long long s = subtreeSums[i];
+            maxProd = max(maxProd, s * (totalSum-s));
+        }
         return maxProd%mod;
     }
 };
