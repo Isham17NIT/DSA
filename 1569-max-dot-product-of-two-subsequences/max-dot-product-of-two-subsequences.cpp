485a44fe19aaed6f76ef195d7b989ca9041731e4
Isham <ishamahuja17@gmail.com>
2026-01-08 11:33:49 +0530

    Time: 13 ms (78.80%) | Memory: 17.7 MB (54.80%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,26 +1,30 @@
 class Solution 
 {
-private:
-    int calculateMaxDotProduct(int idx1, int idx2, vector<int>& nums1, vector<int>& nums2, vector<vector<int>> &dp)
-    {
-        if(idx1==nums1.size() || idx2==nums2.size())
-            return -1e9;
-
-        if(dp[idx1][idx2]!=-1e9)
-            return dp[idx1][idx2];
-
-        int val = nums1[idx1] * nums2[idx2];
-
-        int take_i_j = val + calculateMaxDotProduct(idx1+1, idx2+1, nums1, nums2, dp);
-        int take_j = calculateMaxDotProduct(idx1, idx2+1, nums1, nums2, dp);
-        int take_i = calculateMaxDotProduct(idx1+1, idx2, nums1, nums2, dp);
-    
-        return dp[idx1][idx2]=max({val, take_i, take_j, take_i_j});
-    }
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) 
     {
-        vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size(), -1e9));
-        return calculateMaxDotProduct(0, 0, nums1, nums2, dp);
+        int m=nums1.size(), n=nums2.size();
+        vector<vector<int>> dp(m, vector<int>(n, -1e9));
+        dp[0][0] = nums1[0] * nums2[0];
+        for(int i=1;i<m;i++)
+        {
+            dp[i][0] = max(dp[i-1][0], nums1[i]*nums2[0]);
+        }
+        for(int j=1;j<n;j++)
+        {
+            dp[0][j] = max(dp[0][j-1], nums1[0]*nums2[j]);
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                int val = nums1[i] * nums2[j];
+                int take_i_j = val + dp[i-1][j-1];
+                int take_j = dp[i][j-1];
+                int take_i = dp[i-1][j];
+                dp[i][j] = max({val, take_i_j, take_j, take_i});
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
