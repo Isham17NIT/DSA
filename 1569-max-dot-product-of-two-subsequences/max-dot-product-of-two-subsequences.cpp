2a4a9e9af3c0c85225f4ff4eded5c64453e3060a
Isham <ishamahuja17@gmail.com>
2026-01-08 14:24:46 +0530

    Time: 18 ms (60.40%) | Memory: 17.3 MB (70.00%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -4,27 +4,27 @@ public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) 
     {
         int m=nums1.size(), n=nums2.size();
-        vector<vector<int>> dp(m, vector<int>(n, -1e9));
-        dp[0][0] = nums1[0] * nums2[0];
-        for(int i=1;i<m;i++)
-        {
-            dp[i][0] = max(dp[i-1][0], nums1[i]*nums2[0]);
-        }
+        vector<int> dp(n, -1e9);
+        dp[0] = nums1[0] * nums2[0];
+
         for(int j=1;j<n;j++)
         {
-            dp[0][j] = max(dp[0][j-1], nums1[0]*nums2[j]);
+            dp[j] = max(dp[j-1], nums1[0]*nums2[j]);
         }
         for(int i=1;i<m;i++)
         {
+            vector<int> curr(n, -1e9);
+            curr[0] = max(dp[0], nums1[i]*nums2[0]);
             for(int j=1;j<n;j++)
             {
                 int val = nums1[i] * nums2[j];
-                int take_i_j = val + dp[i-1][j-1];
-                int take_j = dp[i][j-1];
-                int take_i = dp[i-1][j];
-                dp[i][j] = max({val, take_i_j, take_j, take_i});
+                int take_i_j = val + dp[j-1];
+                int take_j = curr[j-1];
+                int take_i = dp[j];
+                curr[j] = max({val, take_i_j, take_j, take_i});
             }
+            dp=curr;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
