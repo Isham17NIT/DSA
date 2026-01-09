64a7cdbb380883b1223670f2c608a25e8ac914f9
Isham <ishamahuja17@gmail.com>
2026-01-09 19:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (80.11%) - LeetSync

--- 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -1,53 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* subtreeWithAllDeepest(TreeNode* root) 
-    {
-        queue<TreeNode*> q;
-        vector<TreeNode*> currLevel;
-        unordered_map<TreeNode*, TreeNode*> m; // node : parent
-        m[root]=nullptr;
-        q.push(root);
-        while(!q.empty())
-        {
-            currLevel.clear();
-            int n=q.size();
-            for(int i=0;i<n;i++)
-            {
-                TreeNode* curr = q.front();
-                q.pop();
-                currLevel.push_back(curr);
-                if(curr->left){
-                    q.push(curr->left);
-                    m[curr->left] = curr;
-                }
-                if(curr->right){
-                    q.push(curr->right);
-                    m[curr->right] = curr;
-                }
-            }
+    pair<TreeNode*, int> dfs(TreeNode* root) {
+        if (!root) return {nullptr, 0};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        if (left.second == right.second) {
+            return {root, left.second + 1};
+        } 
+        else if (left.second > right.second) {
+            return {left.first, left.second + 1};
+        } 
+        else {
+            return {right.first, right.second + 1};
         }
-        while(currLevel.size() > 1){
-            vector<TreeNode*> next;
-            int n=currLevel.size();
-            for(int i=0;i<n;i++)
-            {
-                if(next.empty() || m[currLevel[i]] != next.back()){
-                    next.push_back(m[currLevel[i]]);
-                }
-            }
-            currLevel = next;
-        }
-        return currLevel[0];
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return dfs(root).first;
     }
 };
