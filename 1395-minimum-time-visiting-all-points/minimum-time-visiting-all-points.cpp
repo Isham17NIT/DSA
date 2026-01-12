7e54f9d108886a10b25c36b3fc74473988c25098
Isham <ishamahuja17@gmail.com>
2026-01-12 18:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (26.76%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -4,30 +4,11 @@ public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) 
     {
         int time = 0;
-        vector<int> prev = points[0];
         for(int i=1;i<points.size();i++)
         {
-            int x = points[i][0], y=points[i][1];
-            int dis = min(abs(prev[0]-x), abs(prev[1]-y));
-            if(prev[0] < x && prev[1] < y){
-                prev[0]+=dis;
-                prev[1]+=dis;
-            }          
-            else if(prev[0] < x && prev[1] > y){
-                prev[0] += dis;
-                prev[1] -= dis;
-            }
-            else if(prev[0] > x && prev[1] < y){
-                prev[0] -= dis;
-                prev[1] += dis;
-            }   
-            else if(prev[0] > x && prev[1] > y){
-                prev[0] -= dis;
-                prev[1] -= dis;
-            }
-            time+=abs(dis);
-            time += abs(x-prev[0]) + abs(y-prev[1]);
-            prev = points[i];
+            int dx = abs(points[i][0] - points[i-1][0]);
+            int dy = abs(points[i][1] - points[i-1][1]);
+            time += max(dx, dy);
         }
         return time;
     }
