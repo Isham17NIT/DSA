001dbcc6fe5eeab50c55f6befbe6d045cd1d75bf
Isham <ishamahuja17@gmail.com>
2026-01-18 16:26:21 +0530

    Time: 16 ms (67.44%) | Memory: 14.2 MB (59.88%) - LeetSync

--- 1311-largest-magic-square/largest-magic-square.cpp
+++ 1311-largest-magic-square/largest-magic-square.cpp
@@ -1,13 +1,13 @@
 class Solution {
 private:
-    bool isMagicSquare(vector<vector<int>>& grid, int x, int y, int len)
+    bool isMagicSquare(int x, int y, int m, int n, int len, vector<vector<int>>& rowPrefixSum, vector<vector<int>>& colPrefixSum, vector<vector<int>>& grid)
     {
-        int rowSum=0, colSum=0;
+        int rowSum = y==0 ? rowPrefixSum[x][y+len-1] : rowPrefixSum[x][y+len-1] - rowPrefixSum[x][y-1];
+        int colSum = x==0 ? colPrefixSum[x+len-1][y] : colPrefixSum[x+len-1][y] - colPrefixSum[x-1][y];
+
         int diag1Sum=0, diag2Sum=0;
         for(int i=0;i<len;i++)
         {
-            rowSum += grid[x][y+i];
-            colSum += grid[x+i][y];
             diag1Sum += grid[x+i][y+i];
             diag2Sum += grid[x+i][y+len-1-i];
         }
@@ -16,25 +16,22 @@ private:
 
         for(int i=1;i<len;i++) // rowNum or colNum
         {
-            int currRowSum=grid[x+i][y], currColSum=grid[x][y+i];
-            for(int j=1;j<len;j++)
-            {
-                currRowSum += grid[x+i][y+j];
-                currColSum += grid[x+j][y+i];
-            }
+            int currRowSum = y==0 ? rowPrefixSum[x+i][y+len-1] : rowPrefixSum[x+i][y+len-1] - rowPrefixSum[x+i][y-1];
+            int currColSum = x==0 ? colPrefixSum[x+len-1][y+i] : colPrefixSum[x+len-1][y+i] - colPrefixSum[x-1][y+i];
+        
             if(currRowSum!=rowSum || currColSum!=rowSum)
                 return false;
         }
         return true;
     }
-    bool hasMagicSquare(vector<vector<int>>& grid, int m, int n, int len)
+    bool hasMagicSquare(int m, int n, int len, vector<vector<int>>& rowPrefixSum, vector<vector<int>>& colPrefixSum, vector<vector<int>>& grid)
     {
         // len means we have to look for square of side = len
         for(int i=0;i<=m-len;i++)
         {
             for(int j=0;j<=n-len;j++)
             {
-                if(isMagicSquare(grid, i, j, len))
+                if(isMagicSquare(i, j, m, n, len, rowPrefixSum, colPrefixSum, grid))
                     return true;
             }
         }
@@ -43,15 +40,25 @@ private:
 public:
     int largestMagicSquare(vector<vector<int>>& grid) 
     {
-        int ans = 1;
         int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> rowPrefixSum(m, vector<int>(n, 0));
+        vector<vector<int>> colPrefixSum(m, vector<int>(n, 0));
+
+        for(int i=0;i<m;i++)  // O(m*n)
+        {
+            for(int j=0;j<n;j++)
+            {
+                rowPrefixSum[i][j] = j==0 ? grid[i][j] : grid[i][j] + rowPrefixSum[i][j-1]; 
+                colPrefixSum[i][j] = i==0 ? grid[i][j] : grid[i][j] + colPrefixSum[i-1][j];
+            }
+        }
 
         // check all squares in the matrix and find the largest one
-        for(int i=min(m, n);i>=2;i--)
+        for(int i=min(m, n);i>=2;i--) 
         {
-            if(hasMagicSquare(grid, m, n, i))
+            if(hasMagicSquare(m, n, i, rowPrefixSum, colPrefixSum, grid))
                 return i;
         }
-        return ans;
+        return 1;
     }
 };
